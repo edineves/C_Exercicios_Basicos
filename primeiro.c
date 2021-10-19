434abbffbe6b10069b76511f3845a8c3bdcbf918
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-10-19 11:31:38 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,27 @@
+/*Faça um programa em c que desenhe um "pinheiro"na tela,similar ao abaixo
+*/
+#include <stdio.h>
+
+int main(void) {
+	printf("\n       x            ");
+	printf("\n                    ");
+	printf("\n      xxx           ");
+	printf("\n                    ");
+	printf("\n     xxxxx          ");
+	printf("\n                    ");
+	printf("\n    xxxxxxxx        ");
+	printf("\n                    ");
+	printf("\n   xxxxxxxxxx       ");
+	printf("\n                    ");
+	printf("\n  xxxxxxxxxxxxxx    ");
+	printf("\n                    ");
+	printf("\n xxxxxxxxxxxxxxxxx  ");
+	printf("\n                    ");
+	printf("\n        xx          ");
+	printf("\n                    ");
+	printf("\n        xx          ");
+	printf("\n                    ");
+	printf("\n       xxxx          ");
+	return 0;
+}
+
